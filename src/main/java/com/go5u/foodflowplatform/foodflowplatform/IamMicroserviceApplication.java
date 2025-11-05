c7f679a5aeef42b3095554e680cfc37470b255aa
Romina Maita <u202213765@upc.edu.pe>
2025-11-05 00:12:53 -0500

    feat: add Eureka client configuration and update application properties

--- src/main/java/com/go5u/foodflowplatform/foodflowplatform/IamMicroserviceApplication.java
+++ src/main/java/com/go5u/foodflowplatform/foodflowplatform/IamMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.go5u.foodflowplatform.foodflowplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IamMicroserviceApplication {
 
     public static void main(String[] args) {
