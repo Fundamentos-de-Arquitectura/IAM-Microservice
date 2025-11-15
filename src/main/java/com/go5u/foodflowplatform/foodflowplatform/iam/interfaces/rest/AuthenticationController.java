d3452121684045cbd7e764a1297f34f030cf572f
Romina Maita <u202213765@upc.edu.pe>
2025-11-15 17:31:34 -0500

    fix: correct API request mapping path in AuthenticationController

--- src/main/java/com/go5u/foodflowplatform/foodflowplatform/iam/interfaces/rest/AuthenticationController.java
+++ src/main/java/com/go5u/foodflowplatform/foodflowplatform/iam/interfaces/rest/AuthenticationController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "api/v1/authentication", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/v1/authentication", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Authentication", description = "Authentication Endpoints")
 public class AuthenticationController {
     private final AccountCommandService accountCommandService;
